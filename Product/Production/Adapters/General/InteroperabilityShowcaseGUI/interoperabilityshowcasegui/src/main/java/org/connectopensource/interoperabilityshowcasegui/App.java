404dd7863d38a896e28b5f62259e06473b2a5b6f
Matt Weaver <matthew.weaver@cgifederal.com>
2013-02-06 18:05:19 -0500

    GATEWAY-3455: WIP - to be moved to another repo.

---
+++ Product/Production/Adapters/General/InteroperabilityShowcaseGUI/interoperabilityshowcasegui/src/main/java/org/connectopensource/interoperabilityshowcasegui/App.java
@@ -0,0 +1,13 @@
+package org.connectopensource.interoperabilityshowcasegui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
